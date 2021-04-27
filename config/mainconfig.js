e6894a5c3a73e35b04e531485476a18b61a2d303
Lien_git <chermenschi.ben@gmail.com>
2021-04-27 14:45:14 +0200

    adding config for database connection

---
+++ config/mainconfig.js
@@ -0,0 +1,5 @@
+module.exports = {
+    db: '',
+    db_nickname: 'mlab-db',
+    application_name: 'motivatiemeter'
+}
--- server.js
+++ server.js
@@ -2,7 +2,12 @@ var express = require('express');
 var app = express();
 var mongoose = require('mongoose');
 
-mongoose.connect('mongodb:')
+mongoose.connect('');
+var db= mongoose.connection;
+
+/*
+Testing the server.js
+
 app.get('/ping',function(req,res){
     res.send({ping:'Server is alive yahoo!'});
 });
@@ -11,5 +16,17 @@ app.get('/ping/:id',function(req, res){
     res.send({ping:'hello this is the server and i got ' + req.params.id});
 });
 
+*/
+
+
+
 app.listen(3000);
 console.log('Listen on port 3000...');
+
+db.on('error', function callback () {
+    console.log("Connection error");
+  });
+  
+  db.once('open', function callback () {
+    console.log("Mongo working!");
+  });
