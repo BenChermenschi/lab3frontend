0095d31754b4107f3dd5c9e6bdae528e8fc361b0
Lien_git <chermenschi.ben@gmail.com>
2021-05-04 20:05:35 +0200

    Update surveyRoutes.js

---
+++ routes/surveyRoutes.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const surveyRouter = express.Router();
+
+surveyController = require('..controllers/surveyController');
+
+//testroute
+lesController.get('/',(req,res)=>{
+    res.send('SURVEY');
+});
+
+module.exports = router;
