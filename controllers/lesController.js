1519397ad667d2e7ba7911920be8d1419ab2f8c6
Lien_git <chermenschi.ben@gmail.com>
2021-05-04 12:50:13 +0200

    rough routes

--- controllers/lesController.js
+++ controllers/lesController.js
@@ -1,8 +1,43 @@
 var mongoose = require('mongoose');
-var Les = mongoose.model('Les');
+var les = mongoose.model('Les');
 
 //GET : all lessen
 exports.getAllLessen= function(req,res,next){
     
     //grab all lessen
+    console.log('searching for all Lessen');
+    les.find().exec(function(err,lessen){
+        //on fail
+        if (err){
+            return res.status(500).json({
+                title:'Error occured',
+                error:err
+            });
+        }
+        //on success
+        res.status(200).json({
+            message:'success',
+            obj: les
+        });
+    });
+}
+
+//GET : les by id
+exports.getLesAtId= function(req,res){
+    console.log('searching lessen for _id : ');
+    console.log(req.params.id);
+    les.find({_id:req.params.id}).exec(function(err,responseLes){
+        //on fail
+        if (err){
+            return res.status(500).json({
+                title:'error occured',
+                error:err
+            });
+        }
+        //on success
+        res.status(200).json({
+            message:'Success',
+            object:responseLes
+        });
+    });
 }
