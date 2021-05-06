3af64ee58f7626c1ca2a6cace4ef79c1b0220b68
Lien_git <chermenschi.ben@gmail.com>
2021-05-06 16:37:07 +0200

    rework usermodel

--- models/userModel.js
+++ models/userModel.js
@@ -3,10 +3,9 @@ var Schema = mongoose.Schema;
 var mongooseUniqueValidator = require('mongoose-unique-validator');
 
 var schema = new Schema({
-    id:{type:String, required:true, unique:true},
     email:{type:String,required:true},
-    name:{type:String,required:true},
-    password:{type:String,required:true},
+    naam:{type:String,required:true},
+    wachtwoord:{type:String,required:true},
     usertype:{type:Schema.Types.ObjectId, ref:'UserType'}
 })
 
