9768be1ea597ef4f1af13bd712d3790072b278e4
BenChermenschi <chermenschi.ben@gmail.com>
2021-05-22 13:16:08 +0200

    make interceptor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
@@ -5,6 +6,8 @@ import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
+import { AuthInterceptor } from './core/interceptors/authInterceptor';
+import { AuthService } from './core/services/auth.service';
 import { SharedModule } from './shared/shared.module';
 
 @NgModule({
@@ -18,7 +21,10 @@ import { SharedModule } from './shared/shared.module';
     RouterModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    AuthService,
+    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
