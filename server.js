fcef1f17b0988c48ee1364ab0e9e6a30740ac40b
Lien_git <chermenschi.ben@gmail.com>
2021-06-08 15:17:31 +0200

    deactivating cors options for testing

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ let corsOptions={
 }
 
 //app usages
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 app.use(cookieParser());
