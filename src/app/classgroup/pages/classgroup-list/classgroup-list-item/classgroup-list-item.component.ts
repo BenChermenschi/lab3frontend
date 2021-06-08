a46bc7c8b9f726960f5a765a49887f536b5d71b5
Lien_git <chermenschi.ben@gmail.com>
2021-06-08 20:22:50 +0200

    klasgroep lijst generation works

--- src/app/classgroup/pages/classgroup-list/classgroup-list-item/classgroup-list-item.component.ts
+++ src/app/classgroup/pages/classgroup-list/classgroup-list-item/classgroup-list-item.component.ts
@@ -1,15 +1,17 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { BaseComponent } from 'src/app/core/base/base.component';
 import { Klasgroep } from 'src/app/core/models/klasgroep.model';
 
 @Component({
-  selector: 'app-classgroup-list-item',
+  selector: '[app-classgroup-list-item]',
   templateUrl: './classgroup-list-item.component.html',
   styleUrls: ['./classgroup-list-item.component.sass']
 })
-export class ClassgroupListItemComponent implements OnInit {
+export class ClassgroupListItemComponent extends BaseComponent implements OnInit {
   @Input() klasgroep:Klasgroep ;
   constructor(private router:Router) {
+    super();
     this.klasgroep =  {_id:"",naam:"",aantalStudenten:0};
   }
 
