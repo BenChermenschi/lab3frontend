0a3271ebbb576a1edeff8fcf65af0db985f2eb8f
Lien_git <chermenschi.ben@gmail.com>
2021-06-11 18:49:19 +0200

    vak create done

--- src/app/course/pages/course-create/course-create.component.ts
+++ src/app/course/pages/course-create/course-create.component.ts
@@ -1,15 +1,71 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import { takeUntil } from 'rxjs/operators';
+import { BaseComponent } from 'src/app/core/base/base.component';
+import { APIResponse } from 'src/app/core/models/APIResponse.model';
+import { VakPost } from 'src/app/core/models/vak.model';
+import { VakService } from 'src/app/core/services/vak.service';
 
 @Component({
   selector: 'app-course-create',
   templateUrl: './course-create.component.html',
   styleUrls: ['./course-create.component.sass']
 })
-export class CourseCreateComponent implements OnInit {
+export class CourseCreateComponent extends BaseComponent implements OnInit {
 
-  constructor() { }
+  myform = new FormGroup({});
+  constructor(private vakService:VakService,private router:Router) {
+    super();
+   }
 
   ngOnInit(): void {
+    this.myform = new FormGroup({
+      naam:new FormControl()
+    });
+  }
+
+  clearForm(){
+    this.myform.reset();
+  }
+
+  submit(){
+    this.createVak();
+  }
+
+  createVak(){
+    let naam = this.myform.value.naam;
+
+    const valid = this.validateFields();
+
+    if(valid === true){
+      const newVak : VakPost = {
+        naam:naam
+      }
+    
+
+    this.vakService
+      .create(newVak)
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((response:APIResponse)=>{
+        console.log(response);
+        if (response.message === "vak created") {
+          alert("creatie successvol");
+          this.router.navigate(['/course']);
+        }else{
+          alert("creatie mislukt")
+        }
+      })
+    }
+  }
+
+  validateFields(){
+    let valid = true;
+    if (this.myform.value.naam === null || this.myform.value.naam === "") {
+      valid = false;
+      
+    }
+    return valid;
   }
 
 }
