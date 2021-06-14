9d68f057575ee14c4386ac66166d8f817c09d919
Lien_git <chermenschi.ben@gmail.com>
2021-06-14 17:34:50 +0200

    survey detail : more stuff shown (graph still needs fixing)

--- src/app/survey/pages/survey-detail/survey-detail.component.ts
+++ src/app/survey/pages/survey-detail/survey-detail.component.ts
@@ -1,9 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { JsonPipe } from '@angular/common';
+import { Component, OnInit, ViewChild, ViewChildren } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { ChartDataset, ChartOptions, ChartType } from 'chart.js';
+import { ChartComponent } from 'angular2-chartjs';
+import { Chart, ChartDataset,  ChartOptions, ChartType } from 'chart.js';
 import { Subscription } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
 import { BaseComponent } from 'src/app/core/base/base.component';
+import { Klasgroep } from 'src/app/core/models/klasgroep.model';
+import { Reactie } from 'src/app/core/models/reactie.model';
 import { VragenlijstDetailed } from 'src/app/core/models/vragenLijst.model';
 import { VragenlijstService } from 'src/app/core/services/vragenlijst.service';
 
@@ -17,6 +21,19 @@ import { VragenlijstService } from 'src/app/core/services/vragenlijst.service';
 export class SurveyDetailComponent extends BaseComponent implements OnInit {
 
   //chart stuff
+  
+  
+  @ViewChildren('benMeeChart') chart:ChartComponent | undefined;
+  benMeeType:any;
+  benMeeData:any;
+  benMeeOptions:any;
+  benMeeLabels:any;
+  benMeeLegend:any;
+  benMeePlugins:any;
+
+  
+
+//old
   public barChartLabels =['2006', '2007', '2008', '2009', '2010'];
   public barChartOptions: ChartOptions = {
     responsive: true,
@@ -26,19 +43,24 @@ export class SurveyDetailComponent extends BaseComponent implements OnInit {
   public barChartLegend = true;
   public barChartPlugins = [];
 
-  public barChartData: ChartDataset[] =[
-    {data:[],label:'Huidige antwoorden'}
-  ];
+
+  public barChartData: any[] =[{
+    data:[5,3,4,7,1],label:"aantal antwoorden"
+  }];
   
 
 
   vragenlijst:VragenlijstDetailed | undefined;
   private routeSub:Subscription | undefined;
   Id:string="";
+  welkOnderdeel:string[] = [];
+  andereVragen:string[]=[];
+  respondenten:number=0;
+  respondentenLimit:number=0;
 
   constructor(private route:ActivatedRoute,private vragenlijstService:VragenlijstService) {
     super()
-    this.routeSub = undefined;
+   
   }
 
   ngOnInit(): void {
@@ -59,16 +81,94 @@ export class SurveyDetailComponent extends BaseComponent implements OnInit {
 
   updatebenMee():void{
     if (this.vragenlijst != undefined) {
-      this.barChartData[0].data=[
-        this.vragenlijst?.totalen.benMee.aantal1,
-        this.vragenlijst.totalen.benMee.aantal2,
-        this.vragenlijst.totalen.benMee.aantal3,
-        this.vragenlijst.totalen.benMee.aantal4,
-        this.vragenlijst.totalen.benMee.aantal5
-      ]
+      console.log("updatingBenMee");
+      /*
+        this.benMeeType='bar';
+        this.benMeeLegend = true;
+        this.benMeeData={
+          labels:["Helemaal niet","niet mee","half half","grotendeels mee","volledig mee"],
+          datasets:[ 
+            
+            this.vragenlijst.totalen.benMee.aantal1,
+            this.vragenlijst.totalen.benMee.aantal2,
+            this.vragenlijst.totalen.benMee.aantal3,
+            this.vragenlijst.totalen.benMee.aantal4,
+            this.vragenlijst.totalen.benMee.aantal5
+          ]
+        };
+        this.benMeeOptions={
+          responsive: true,
+        };
+
+*/
+
+this.benMeeType = 'horizontalBar';
+		this.benMeeData = {
+			labels: ["5 star", "4 start", "3 start", "2 star", "1 star"],
+			datasets: [
+				{
+					fill: true,
+					label: false,
+					backgroundColor: ["#a1a7b3", "#a1a7b3", "#a1a7b3", "#a1a7b3", "#a1a7b3"],
+					data: [10, 2, 5, 4, 5]
+				}
+			]
+		}
+		this.benMeeOptions = {
+			maintainAspectRatio: false,
+			responsive: false,
+			legend: { display: false },
+			title: {
+				display: false,
+				text: ''
+			},
+			scales:
+				{
+					yAxes: [{
+						// barPercentage: 0.4,
+						barThickness: 20,
+						barPercentage: .5,
+						categoryPercentage: .2,
+						isFixedWidth: true,
+						//Number - Pixel width of the bar
+						barWidth: 20,
+						gridLines: {
+							display: false
+						},
+						ticks: {
+							min: 0,
+							stepSize: 1,
+							fixedStepSize: 1,
+						}
+					}],
+					xAxes: [{
+						display: false,
+						gridLines: {
+							display: false
+						},
+						ticks: {
+							min: 0,
+							stepSize: 1,
+							fixedStepSize: 1,
+						}
+					}],
+				}
+		}
+	
+
+
+        }
+        
+      
+      
+
+      
+    
     }
     
-  }
+  
+
+ 
 
   getVragenlijstDetailed():void{
     this.vragenlijstService
@@ -78,11 +178,60 @@ export class SurveyDetailComponent extends BaseComponent implements OnInit {
       this.vragenlijst = response;
       console.log(response);
       console.log(this.vragenlijst);
+
       this.updateGraphs();
+
+      this.welkOnderdeel = this.filterWelkOnderdeel(this.vragenlijst.reacties);
+      this.andereVragen = this.filterAndereVragen(this.vragenlijst.reacties);
+      this.respondenten = this.berekenRespondenten(this.vragenlijst.reacties);
+      this.respondentenLimit = this.berekenRespondentenUpperlimit(this.vragenlijst.klasgroepen);
+
     })
-
-
   }
+
+  filterWelkOnderdeel(reacties:Reactie[]){
+    let resultaat:string[] = [];
+    reacties.forEach(reactie => {
+      if (reactie.welkOnderdeel != "") {
+        resultaat.push(reactie.welkOnderdeel)
+      }
+    });
+    return resultaat;
+  }
+
+  filterAndereVragen(reacties:Reactie[]){
+    let resultaat:string[] = [];
+    reacties.forEach(reactie => {
+      if (reactie.andereVragen != "") {
+        resultaat.push(reactie.andereVragen)
+      }
+    });
+    return resultaat;
+  }
+
+  berekenRespondenten(reacties:Array<Reactie>){
+    return reacties.length;
+  }
+
+  berekenRespondentenUpperlimit(klasgroepen:Array<Klasgroep>){
+    let resultaat = 0;
+    klasgroepen.forEach(klasgroep => {
+      resultaat +=klasgroep.aantalStudenten;
+    });
+    return resultaat;
+  }
+
+/*
+  filterRemoveEmpty(inputArray:string[]){
+    let resultaat:string[] = [];
+    inputArray.forEach(element => {
+      if (element != "") {
+        resultaat.push(element)
+      }
+    });
+    return resultaat;
+  }
+  */
 
 
   prepareGraphs(){
