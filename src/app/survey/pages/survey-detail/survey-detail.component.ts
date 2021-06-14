cecb94125b8057ef0bc3c4f9ec04c0aec7fd3ada
Lien_git <chermenschi.ben@gmail.com>
2021-06-14 21:34:20 +0200

    Graphs are HEEEEEEERRRREEEEE

--- src/app/survey/pages/survey-detail/survey-detail.component.ts
+++ src/app/survey/pages/survey-detail/survey-detail.component.ts
@@ -2,7 +2,7 @@ import { JsonPipe } from '@angular/common';
 import { Component, OnInit, ViewChild, ViewChildren } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ChartComponent } from 'angular2-chartjs';
-import { Chart, ChartDataset,  ChartOptions, ChartType } from 'chart.js';
+import { Chart, ChartDataset, ChartOptions, ChartType } from 'chart.js';
 import { Subscription } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
 import { BaseComponent } from 'src/app/core/base/base.component';
@@ -21,50 +21,51 @@ import { VragenlijstService } from 'src/app/core/services/vragenlijst.service';
 export class SurveyDetailComponent extends BaseComponent implements OnInit {
 
   //chart stuff
+
+
+  @ViewChildren('benMeeChart') chart: any | undefined;
+  benMeeType: any;
+  benMeeData: any;
+  benMeeOptions: any;
+  benMeeLabels: any;
+  benMeeLegend: any;
+  benMeePlugins: any;
+
+
+
+  //old
+  /*
+    public barChartLabels =['2006', '2007', '2008', '2009', '2010'];
+    public barChartOptions: ChartOptions = {
+      responsive: true,
+    };
+    
+    public barChartType: ChartType = 'bar';
+    public barChartLegend = true;
+    public barChartPlugins = [];
   
   
-  @ViewChildren('benMeeChart') chart:ChartComponent | undefined;
-  benMeeType:any;
-  benMeeData:any;
-  benMeeOptions:any;
-  benMeeLabels:any;
-  benMeeLegend:any;
-  benMeePlugins:any;
-
-  
-
-//old
-  public barChartLabels =['2006', '2007', '2008', '2009', '2010'];
-  public barChartOptions: ChartOptions = {
-    responsive: true,
-  };
-  
-  public barChartType: ChartType = 'bar';
-  public barChartLegend = true;
-  public barChartPlugins = [];
+    public barChartData: any[] =[{
+      data:[5,3,4,7,1],label:"aantal antwoorden"
+    }];*/
 
 
-  public barChartData: any[] =[{
-    data:[5,3,4,7,1],label:"aantal antwoorden"
-  }];
-  
 
+  vragenlijst: VragenlijstDetailed | undefined;
+  private routeSub: Subscription | undefined;
+  Id: string = "";
+  welkOnderdeel: string[] = [];
+  andereVragen: string[] = [];
+  respondenten: number = 0;
+  respondentenLimit: number = 0;
 
-  vragenlijst:VragenlijstDetailed | undefined;
-  private routeSub:Subscription | undefined;
-  Id:string="";
-  welkOnderdeel:string[] = [];
-  andereVragen:string[]=[];
-  respondenten:number=0;
-  respondentenLimit:number=0;
-
-  constructor(private route:ActivatedRoute,private vragenlijstService:VragenlijstService) {
+  constructor(private route: ActivatedRoute, private vragenlijstService: VragenlijstService) {
     super()
-   
+
   }
 
   ngOnInit(): void {
-    this.routeSub=this.route.params.pipe(takeUntil(this.destroy$)).subscribe(params=>{
+    this.routeSub = this.route.params.pipe(takeUntil(this.destroy$)).subscribe(params => {
       console.log(params['id']);
       this.Id = params['id'];
       this.getVragenlijstDetailed();
@@ -75,122 +76,87 @@ export class SurveyDetailComponent extends BaseComponent implements OnInit {
 
   }
 
-  updateGraphs():void{
+  updateGraphs(): void {
     this.updatebenMee();
   }
 
-  updatebenMee():void{
+  updatebenMee(): void {
     if (this.vragenlijst != undefined) {
       console.log("updatingBenMee");
-      /*
-        this.benMeeType='bar';
-        this.benMeeLegend = true;
-        this.benMeeData={
-          labels:["Helemaal niet","niet mee","half half","grotendeels mee","volledig mee"],
-          datasets:[ 
-            
-            this.vragenlijst.totalen.benMee.aantal1,
-            this.vragenlijst.totalen.benMee.aantal2,
-            this.vragenlijst.totalen.benMee.aantal3,
-            this.vragenlijst.totalen.benMee.aantal4,
-            this.vragenlijst.totalen.benMee.aantal5
-          ]
-        };
-        this.benMeeOptions={
-          responsive: true,
-        };
-
-*/
-
-this.benMeeType = 'horizontalBar';
-		this.benMeeData = {
-			labels: ["5 star", "4 start", "3 start", "2 star", "1 star"],
-			datasets: [
-				{
-					fill: true,
-					label: false,
-					backgroundColor: ["#a1a7b3", "#a1a7b3", "#a1a7b3", "#a1a7b3", "#a1a7b3"],
-					data: [10, 2, 5, 4, 5]
-				}
-			]
-		}
-		this.benMeeOptions = {
-			maintainAspectRatio: false,
-			responsive: false,
-			legend: { display: false },
-			title: {
-				display: false,
-				text: ''
-			},
-			scales:
-				{
-					yAxes: [{
-						// barPercentage: 0.4,
-						barThickness: 20,
-						barPercentage: .5,
-						categoryPercentage: .2,
-						isFixedWidth: true,
-						//Number - Pixel width of the bar
-						barWidth: 20,
-						gridLines: {
-							display: false
-						},
-						ticks: {
-							min: 0,
-							stepSize: 1,
-							fixedStepSize: 1,
-						}
-					}],
-					xAxes: [{
-						display: false,
-						gridLines: {
-							display: false
-						},
-						ticks: {
-							min: 0,
-							stepSize: 1,
-							fixedStepSize: 1,
-						}
-					}],
-				}
-		}
-	
+      const canvas = <HTMLCanvasElement>document.getElementById('benMeeChart');
+      let ctx: any;
+      if (canvas.getContext('2d') != null) {
+        ctx = canvas.getContext('2d');
+      }
 
 
+      let myChart = new Chart(ctx, {
+
+        type: 'bar',
+        data: {
+          labels: ['Absoluut Niet ', 'Ik snap er zo goed als niets van', 'Half en Half, moet het nog eens bekijken', 'Ik snap de meerderheid', 'Ik ben volledig mee'],
+          datasets: [{
+            label: '# of Votes',
+            data: [this.vragenlijst.totalen.benMee.aantal1, this.vragenlijst.totalen.benMee.aantal2, this.vragenlijst.totalen.benMee.aantal3, this.vragenlijst.totalen.benMee.aantal4, this.vragenlijst.totalen.benMee.aantal5],
+            backgroundColor: [
+              'rgba(255, 99, 132, 0.2)',
+              'rgba(54, 162, 235, 0.2)',
+              'rgba(255, 206, 86, 0.2)',
+              'rgba(75, 192, 192, 0.2)',
+              'rgba(153, 102, 255, 0.2)',
+              'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+              'rgba(255, 99, 132, 1)',
+              'rgba(54, 162, 235, 1)',
+              'rgba(255, 206, 86, 1)',
+              'rgba(75, 192, 192, 1)',
+              'rgba(153, 102, 255, 1)',
+              'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+          }]
+        },
+        options: {
+          scales: {
+            y: {
+              beginAtZero: true
+            }
+          }
         }
-        
-      
-      
+      })
 
-      
-    
     }
-    
-  
 
- 
 
-  getVragenlijstDetailed():void{
-    this.vragenlijstService
-    .getById(this.Id)
-    .pipe(takeUntil(this.destroy$))
-    .subscribe((response:VragenlijstDetailed)=>{
-      this.vragenlijst = response;
-      console.log(response);
-      console.log(this.vragenlijst);
 
-      this.updateGraphs();
-
-      this.welkOnderdeel = this.filterWelkOnderdeel(this.vragenlijst.reacties);
-      this.andereVragen = this.filterAndereVragen(this.vragenlijst.reacties);
-      this.respondenten = this.berekenRespondenten(this.vragenlijst.reacties);
-      this.respondentenLimit = this.berekenRespondentenUpperlimit(this.vragenlijst.klasgroepen);
-
-    })
   }
 
-  filterWelkOnderdeel(reacties:Reactie[]){
-    let resultaat:string[] = [];
+
+
+
+
+  getVragenlijstDetailed(): void {
+    this.vragenlijstService
+      .getById(this.Id)
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((response: VragenlijstDetailed) => {
+        this.vragenlijst = response;
+        console.log(response);
+        console.log(this.vragenlijst);
+
+        this.updateGraphs();
+
+        this.welkOnderdeel = this.filterWelkOnderdeel(this.vragenlijst.reacties);
+        this.andereVragen = this.filterAndereVragen(this.vragenlijst.reacties);
+        this.respondenten = this.berekenRespondenten(this.vragenlijst.reacties);
+        this.respondentenLimit = this.berekenRespondentenUpperlimit(this.vragenlijst.klasgroepen);
+
+      })
+  }
+
+  filterWelkOnderdeel(reacties: Reactie[]) {
+    let resultaat: string[] = [];
     reacties.forEach(reactie => {
       if (reactie.welkOnderdeel != "") {
         resultaat.push(reactie.welkOnderdeel)
@@ -199,8 +165,8 @@ this.benMeeType = 'horizontalBar';
     return resultaat;
   }
 
-  filterAndereVragen(reacties:Reactie[]){
-    let resultaat:string[] = [];
+  filterAndereVragen(reacties: Reactie[]) {
+    let resultaat: string[] = [];
     reacties.forEach(reactie => {
       if (reactie.andereVragen != "") {
         resultaat.push(reactie.andereVragen)
@@ -209,54 +175,54 @@ this.benMeeType = 'horizontalBar';
     return resultaat;
   }
 
-  berekenRespondenten(reacties:Array<Reactie>){
+  berekenRespondenten(reacties: Array<Reactie>) {
     return reacties.length;
   }
 
-  berekenRespondentenUpperlimit(klasgroepen:Array<Klasgroep>){
+  berekenRespondentenUpperlimit(klasgroepen: Array<Klasgroep>) {
     let resultaat = 0;
     klasgroepen.forEach(klasgroep => {
-      resultaat +=klasgroep.aantalStudenten;
+      resultaat += klasgroep.aantalStudenten;
     });
     return resultaat;
   }
 
-/*
-  filterRemoveEmpty(inputArray:string[]){
-    let resultaat:string[] = [];
-    inputArray.forEach(element => {
-      if (element != "") {
-        resultaat.push(element)
-      }
-    });
-    return resultaat;
-  }
-  */
+  /*
+    filterRemoveEmpty(inputArray:string[]){
+      let resultaat:string[] = [];
+      inputArray.forEach(element => {
+        if (element != "") {
+          resultaat.push(element)
+        }
+      });
+      return resultaat;
+    }
+    */
 
 
-  prepareGraphs(){
+  prepareGraphs() {
     this.prepareGraphBenMee();
     this.prepareAndereVragen();
   }
 
-  prepareComments(){
+  prepareComments() {
     this.prepareTeHerhalen();
     this.prepareAndereVragen();
   }
 
-  prepareGraphBenMee(){
+  prepareGraphBenMee() {
 
   }
 
-  prepareGraphHerhaalLes(){
+  prepareGraphHerhaalLes() {
 
   }
 
-  prepareTeHerhalen(){
+  prepareTeHerhalen() {
 
   }
 
-  prepareAndereVragen(){
+  prepareAndereVragen() {
 
   }
 
