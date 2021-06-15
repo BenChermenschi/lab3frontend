0442ad6d8ccfa13e18e920c16c645c0fdf0f36c0
Lien_git <chermenschi.ben@gmail.com>
2021-06-15 22:20:45 +0200

    base of checking if admin need to filter better

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { BaseComponent } from '../core/base/base.component';
+import { AuthService } from '../core/services/auth.service';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.sass']
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent extends BaseComponent implements OnInit {
 
-  constructor() { }
+  isAdministrator:boolean | undefined;
+
+  constructor(private authService:AuthService) {
+    super();
+   }
 
   ngOnInit(): void {
+    this.checkIfAdmin();
+
+  }
+
+  checkIfAdmin(){
+    this.isAdministrator =this.authService.isAdmin();
+    
   }
 
 }
