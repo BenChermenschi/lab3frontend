e5aeb999c8d14d5c20a859f7bc3a22ac9fe14f0e
Lien_git <chermenschi.ben@gmail.com>
2021-06-16 12:54:02 +0200

    added eventlistener for navbar

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NavigationStart, Router, RouterEvent } from '@angular/router';
+import { filter, takeUntil } from 'rxjs/operators';
 import { BaseComponent } from '../core/base/base.component';
 import { AuthService } from '../core/services/auth.service';
 
@@ -11,18 +13,28 @@ export class NavbarComponent extends BaseComponent implements OnInit {
 
   isAdministrator:boolean | undefined;
 
-  constructor(private authService:AuthService) {
+  constructor(private authService:AuthService, private router:Router) {
     super();
    }
 
   ngOnInit(): void {
-    this.checkIfAdmin();
+    this.routerEventListener();
 
   }
 
   checkIfAdmin(){
     this.isAdministrator =this.authService.isAdmin();
-    
+  }
+
+  routerEventListener(){
+    this.router.events
+    .pipe(
+      filter(event => event instanceof NavigationStart),
+      takeUntil(this.destroy$)
+      )
+    .subscribe(event =>{
+      this.checkIfAdmin();
+    });
   }
 
 }
